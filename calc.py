e927117b120937f68816bfb6459ba0bb2ebc64c4
Vipin Mittal <1729mittalvipin@gmail.com>
2020-01-23 11:04:13 +0530

    henkn

--- calc.py
+++ calc.py
@@ -1,11 +1,27 @@
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 def add(x,y)
+=======
+def add(x,y):
+>>>>>>> Stashed changes
+=======
+def add(x,y):
+>>>>>>> Stashed changes
     return x+y;
 
-def multiply(x,y)
+def multiply(x,y):
     pass
 
-def divide(x,y)
+def divide(x,y):
     pass
 
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 def subtract(x,y)
+=======
+def subtract(x,y):
+>>>>>>> Stashed changes
+=======
+def subtract(x,y):
+>>>>>>> Stashed changes
     pass 
